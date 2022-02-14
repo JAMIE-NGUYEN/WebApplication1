4375e0f57d29275357e5628fc7ef31a93c6da1e3
jamie <jn48097@gmail.com>
2022-02-14 14:20:47 +0800

    Add project files.

---
+++ WebApplication1/Create.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Create.aspx.cs" Inherits="WebApplication1.Create" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+         <asp:TextBox ID="txtDate" runat="server" TextMode="Date" CssClass="form-control input-sm" Width="345px"></asp:TextBox>
+        <asp:DropDownList ID="ddlStartTime" runat="server" CssClass="form-control" AppendDataBoundItems="false" Width="350px">
+                    <asp:ListItem></asp:ListItem>
+                     <asp:ListItem Value="3:00">3:00 AM</asp:ListItem>
+
+                    <asp:ListItem Value="9:00">9:00 AM</asp:ListItem>
+                    <asp:ListItem Value="10:00">10:00 AM</asp:ListItem>
+                    <asp:ListItem Value="11:00">11:00 AM</asp:ListItem>
+                    <asp:ListItem Value="12:00">12:00 AM</asp:ListItem>
+                    <asp:ListItem Value="13:00">1:00 PM</asp:ListItem>
+                    <asp:ListItem Value="14:00">2:00 PM</asp:ListItem>
+                    <asp:ListItem Value="15:00">3:00 PM</asp:ListItem>
+                    <asp:ListItem Value="16:00">4:00 PM</asp:ListItem>
+                    <asp:ListItem Value="17:00">5:00 PM</asp:ListItem>
+                    <asp:ListItem Value="18:00">6:00 PM</asp:ListItem>
+                    <asp:ListItem Value="19:00">7:00 PM</asp:ListItem>
+                </asp:DropDownList>    <br />        
+            <asp:Button ID="Button1" runat="server" Text="Create" OnClick="Button1_Click" />
+
+        </div>
+          <div>
+            <h2>Host URL</h2>
+            <asp:Label ID="Host" runat="server" Text="Link"></asp:Label>
+            <h2>Join URL</h2>
+            <asp:Label ID="Join" runat="server" Text="Link"></asp:Label>
+            <h2>Response Code</h2>
+            <asp:Label ID="Code" runat="server" Text="Code"></asp:Label>
+            <br />
+            <br />
+         </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/Create.aspx.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Create : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            txtDate.Text = DateTime.Now.AddDays(1).ToString("yyyy-MM-dd");
+            txtDate.Attributes["min"] = DateTime.Now.AddDays(1).ToString("yyyy-MM-dd");
+            txtDate.Attributes["max"] = DateTime.Now.AddDays(15).ToString("yyyy-MM-dd");
+            //DateTime dtCurrentDate = DateTime.Today;
+            //DateTime dtCurrentTime = DateTime.Now;
+            //ListItem listItem1 = new ListItem(dtCurrentTime.ToShortTimeString(), dtCurrentTime.ToShortTimeString());
+            //listItem1.Enabled = listItem1.Selected = true;
+
+            //for (int i = 0; i <= 12; i++)
+            //{
+            //    ListItem listItem2 = new ListItem(dtCurrentDate.ToShortTimeString(), dtCurrentDate.ToShortTimeString());
+            //    listItem2.Selected = false;
+            //    ddlStartTime.Items.Add(listItem2);
+            //    if (dtCurrentDate.CompareTo(dtCurrentTime) < 0 && dtCurrentDate.CompareTo(dtCurrentTime) > 0)
+            //        ddlStartTime.Items.Add(listItem1);
+            //    dtCurrentDate = dtCurrentDate.AddMinutes(30);
+            //}
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DateTime st = DateTime.Parse(ddlStartTime.SelectedValue);
+            string nst = st.ToString("H:mm");
+            string date = txtDate.Text;
+            DateTime combined = DateTime.Parse(date + ' ' + nst);
+
+            var tokenHandler = new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler();
+            var now = DateTime.UtcNow;
+            var apiSecret = "TWGXilIqPwb5k4MO1NvvQxX87hdJALogJx9x";
+            byte[] symmetricKey = Encoding.ASCII.GetBytes(apiSecret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = "62ic91AzQ_-C6klfk4OSDw",
+                Expires = now.AddSeconds(300),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(symmetricKey), SecurityAlgorithms.HmacSha256),
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+
+            var client = new RestClient("https://api.zoom.us/v2/users/Appdevproject9@gmail.com/meetings");
+            var request = new RestRequest(Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new { topic = "Meeting with Tutor", duration = "60", start_time = combined, type = "2" });
+            request.AddHeader("authorization", String.Format("Bearer {0}", tokenString));
+
+
+            IRestResponse restResponse = client.Execute(request);
+            HttpStatusCode statusCode = restResponse.StatusCode;
+            int numericStatusCode = (int)statusCode;
+            var jObject = JObject.Parse(restResponse.Content);
+            Host.Text = (string)jObject["start_url"];
+            Join.Text = (string)jObject["join_url"];
+            Code.Text = Convert.ToString(numericStatusCode);
+        }
+    }
+}
